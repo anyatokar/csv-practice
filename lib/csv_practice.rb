14b2abb61e0666619d1fc2df97ef90269e176ed5
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-09-17 16:11:44 -0700

    added responses to comments

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,8 +1,6 @@
 require 'csv'
 require 'awesome_print'
 
-
-
 def get_all_olympic_athletes(filename)
   required_fields = ["ID", "Name", "Height", "Team", "Year", "City", "Sport", "Event", "Medal"]
   athlete_array_of_hashes = CSV.read(filename, headers: true).map { |row| row.to_h} # {&:to_h}
@@ -33,4 +31,30 @@ def get_all_gold_medalists(athlete_array_of_hashes)
   end
   return gold_medalist_array
 end
+
+
+# Write descriptions for 2 test cases that test the core functionality, and are nominal tests.
+#   1) each hash doesn't include any key that isn't required
+#   2) each hash includes the correct number of key-value pairs
+
+# In the tests, how do we "Arrange" and setup the data of all Olympic athletes?
+#   olympic_athletes = get_all_olympic_athletes(OLYMPIC_DATA_FILENAME)
+#
+# How did we "Assert" and check that the method returns a hash?
+#   expect(athlete).must_be_instance_of Hash
+#
+# How did we "Assert" and check that the method returns an accurate hash?
+#    there's already an expected totals hash with the values calculated.
+#    it is used to compare against:
+#       expected_totals.each do |expected_team, expected_count|
+#         expect(total_medals[expected_team]).must_equal expected_count
+#
+# What nominal tests did we miss?
+#   could test to see that select athletes are included.
+#   could test medal counts for a specific country
+#
+# Write descriptions for 2 edge test cases that aren't in our tests. (Don't write the tests, just come up with your test cases for practice!)
+# TDD the implementation of total_medals_per_team(olympic_data) with the provided tests.
+#   1) test for misspelling
+#   2) test for different capitalization of the same thing
 
--- test/csv_practice_test.rb
+++ test/csv_practice_test.rb
@@ -1,28 +1,3 @@
-# Write descriptions for 2 test cases that test the core functionality, and are nominal tests.
-#   1) each hash doesn't include any key that isn't required
-#   2) each hash includes the correct number of key-value pairs
-
-# In the tests, how do we "Arrange" and setup the data of all Olympic athletes?
-#   olympic_athletes = get_all_olympic_athletes(OLYMPIC_DATA_FILENAME)
-#
-# How did we "Assert" and check that the method returns a hash?
-#   expect(athlete).must_be_instance_of Hash
-#
-# How did we "Assert" and check that the method returns an accurate hash?
-#    there's already an expected totals hash with the values calculated.
-#    it is used to compare against:
-#       expected_totals.each do |expected_team, expected_count|
-#         expect(total_medals[expected_team]).must_equal expected_count
-#
-# What nominal tests did we miss?
-#   could test to see that select athletes are included.
-#   could test medal counts for a specific country
-#
-# Write descriptions for 2 edge test cases that aren't in our tests. (Don't write the tests, just come up with your test cases for practice!)
-# TDD the implementation of total_medals_per_team(olympic_data) with the provided tests.
-#   1) test for misspelling
-#   2) test for different capitalization of the same thing
-
 # csv_practice_test.rb
 
 require 'minitest'
